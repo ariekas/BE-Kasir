14af8cdd18c2f55ccd4210bc4daf3643803eac9c
ariekas <ariariekas@gmail.com>
2025-03-19 16:20:32 +0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
